aec1206326006d61e0434e51c329d707ef9a7ce2
sammekristensen <samme.kristensen@hotmail.com>
2022-10-13 18:04:33 +0200

    fib_py

--- MA4/person.py
+++ MA4/person.py
@@ -20,3 +20,9 @@ class Person(object):
         
 	def __del__(self):
 		return lib.Person_delete(self.obj)
+
+def fib_py(age):
+	if age <=1:
+		return age
+	else:
+		return(fib_py(age-1) - fib_py(age-2))
