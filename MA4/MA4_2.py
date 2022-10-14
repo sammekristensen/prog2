41b3605257578f3887fb6d37051ef773d54eeb5f
sammekristensen <samme.kristensen@hotmail.com>
2022-10-14 13:20:01 +0200

    m

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -3,8 +3,16 @@
 from tkinter import N
 from person import Person
 from time import perf_counter as pc
+from numba import njit
 
 def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return (fib_py(n-1) + fib_py(n-2))
+
+@njit
+def fib_numba(n):
 	if n <= 1:
 		return n
 	else:
@@ -18,7 +26,7 @@ def main():
 	#print(f.get())
 
 	start1=pc()
-	print(f"fib of {n} is, valculated in c++: {f.fib()}")
+	print(f"fib of {n} is, calculated in c++: {f.fib()}")
 	end1=pc()
 	print(f"time for c++: {round(end1-start1,2)}")
 
@@ -26,6 +34,8 @@ def main():
 	print(f"fib of {n} is:{fib_py(n)}")
 	end2=pc()
 	print(f"time for fib_py, calculated in py: {round(end2-start2,2)}")
+
+	print(f"fib of {n} is:{fib_numba(n)}")
 
 if __name__ == '__main__':
 	main()
