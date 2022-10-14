31df5e2c2a7d5c9401c88d2a5a80b225bd407abf
sammekristensen <samme.kristensen@hotmail.com>
2022-10-14 13:13:53 +0200

    b

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -18,14 +18,14 @@ def main():
 	#print(f.get())
 
 	start1=pc()
-	print(f"fib of {n} is: {f.fib()}")
+	print(f"fib of {n} is, valculated in c++: {f.fib()}")
 	end1=pc()
 	print(f"time for c++: {round(end1-start1,2)}")
 
 	start2=pc()
-	print(fib_py(n))
+	print(f"fib of {n} is:{fib_py(n)}")
 	end2=pc()
-	print(f"time for fib_py: {round(end2-start2,2)}")
+	print(f"time for fib_py, calculated in py: {round(end2-start2,2)}")
 
 if __name__ == '__main__':
 	main()
