88046bc58090b1f7acac4d52951a7dceb60a0448
sammekristensen <samme.kristensen@hotmail.com>
2022-10-14 11:35:18 +0200

    test fib_py

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+from tkinter import N
 from person import Person
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return (fib_py(n-1) + fib_py(n-1))
 
 def main():
 	n=5
@@ -9,6 +16,7 @@ def main():
 	f.set(9)
 	print(f.get())
 	print(f"fib of {n} is: {f.fib()}")
+	print(fib_py(n))
 
 if __name__ == '__main__':
 	main()
