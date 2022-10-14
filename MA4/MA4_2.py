032a562df0da0c22c8ef201eb53ddc249392f671
sammekristensen <samme.kristensen@hotmail.com>
2022-10-14 14:15:41 +0200

    f

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -51,9 +51,9 @@ def main():
 		fib_numba_lst.append(time3)
 	
 	fig, axs = plt.subplots(3)
-	axs[0].stem(n,fib_py_lst)
-	axs[1].stem(n,fib_numba_lst)
-	axs[2].stem(n,fib_cpp_lst)
+	axs[0].plot(n,fib_py_lst)
+	axs[1].plot(n,fib_numba_lst)
+	axs[2].plot(n,fib_cpp_lst)
 	fig.savefig("the_barchart.jpeg")
 
 	
